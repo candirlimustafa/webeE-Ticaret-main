f9fb7b79de1a12e4fd09b9afe611912dec0e21a6
Mustafa <72815335+candirlimustafa@users.noreply.github.com>
2023-06-12 15:13:28 +0300

    acordion

--- js/custom/extra.js
+++ js/custom/extra.js
@@ -22,3 +22,21 @@ window.addEventListener("DOMContentLoaded", event => {
 });
 
 
+var acco = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acco.length; i++) {
+  acco[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel /
+    this.classList.toggle("active");
+
+    / Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
