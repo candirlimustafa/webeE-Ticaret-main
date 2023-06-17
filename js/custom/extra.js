41fe6def2a797a3514babafd5971d29588f8fbcc
Mikail Akar <huseyin.akar@bil.omu.edu.tr>
2023-06-17 13:36:40 +0300

    Update extra.js

--- js/custom/extra.js
+++ js/custom/extra.js
@@ -22,3 +22,17 @@ window.addEventListener("DOMContentLoaded", event => {
 });
 
 
+let listElements = document.querySelectorAll('.link');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', ()=>{
+        if (listElement.classList.contains('active')){
+            listElement.classList.remove('active');
+        }else{
+            listElements.forEach (listE => {
+                listE.classList.remove('active');
+            })
+            listElement.classList.toggle('active');
+        }
+    })
+});
